bf4b41fb29080c172450deadde8a1bb0914936bf
sad611 <bixincorrendo@gmail.com>
2025-03-04 09:54:50 -0300

    Some cleaning

--- dashboard/src/app/components/login-screen/login-screen.component.ts
+++ dashboard/src/app/components/login-screen/login-screen.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login-screen',
@@ -16,6 +17,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
+    MatSnackBarModule,
   ],
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.scss',
@@ -23,11 +25,21 @@ import { MatIconModule } from '@angular/material/icon';
 export class LoginScreenComponent {
   username: string = '';
   password: string = '';
+  error: boolean = true;
+  duration: number = 5000;
+
+  private _snackBar = inject(MatSnackBar);
 
   register() {
-    console.log('', {
-      username: this.username,
-      password: this.password,
+    console.log(this.username, this.password);
+    if (this.error) {
+      this.openSnackBar()
+    }
+  }
+
+  openSnackBar() {
+    this._snackBar.open('Display error message', 'Dismiss', {
+      duration: this.duration,
     });
   }
 }
