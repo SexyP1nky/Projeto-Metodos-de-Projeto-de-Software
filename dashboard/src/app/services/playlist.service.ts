72f47dd3de38531961f1cf213bff2cd3d6233c8f
sad611 <bixincorrendo@gmail.com>
2025-04-26 09:56:00 -0300

    aaaa

--- dashboard/src/app/services/playlist.service.ts
+++ dashboard/src/app/services/playlist.service.ts
@@ -5,17 +5,16 @@ import { Track } from '../models/track';
 import { RepositoryService } from './repository/repository.service';
 import { ReportService } from './report/report.service';
 import { Observable, Subject, Subscription } from 'rxjs';
-
+import { PlaylistDecorator } from '../models/playlistDecorator';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlaylistService {
-  
   user!: User;
   users!: User[];
 
-  usersSub!: Subscription
+  usersSub!: Subscription;
 
   user$!: Subject<User[]>;
 
@@ -24,14 +23,14 @@ export class PlaylistService {
 
     this.usersSub = this.user$.subscribe((users) => {
       if (users) {
-        this.user = users[0]
+        this.user = users[0];
         this.users = users;
       }
-    })
+    });
 
     const user = new User('samuel', '123', '123', new Date(), false);
 
-    this.updateUser$([user])
+    this.updateUser$([user]);
   }
 
   getUser$(): Subject<User[]> {
@@ -39,32 +38,37 @@ export class PlaylistService {
   }
 
   updateUser$(users: User[]) {
-    this.user$.next(users)
+    this.user$.next(users);
   }
 
   createNewPlaylist(title: string): void {
-      const playlist = new Playlist(title, this.user.id);
-      playlist.tracks.push(new Track('New Jeans', 300000, 'youtube'));
+    let playlist = new Playlist(title, this.user.id);
 
-      this.user.playlists.set(title, playlist);
+    playlist = new PlaylistDecorator(playlist)
+      .addTrack(
+        `NewJeans (뉴진스) 'Right Now' Official MV`,
+        300000,
+        'https://youtu.be/m6pTbEz4w3o'
+      )
+      .getPlaylist();
 
-      this.repository.create(playlist);
+    this.user.playlists.set(title, playlist);
+    this.repository.create(playlist);
   }
 
   deletePlaylist(title: string): boolean {
-      // DB não está implementado
-      this.user.playlists.delete(title);
-      const playlist = this.repository.findByID(title);
-      if (playlist) {
-          this.repository.delete(playlist);
-          return true;
-      }
-      return false;
+    // DB não está implementado
+    this.user.playlists.delete(title);
+    const playlist = this.repository.findByID(title);
+    if (playlist) {
+      this.repository.delete(playlist);
+      return true;
+    }
+    return false;
   }
 
   renamePlaylist(playlist: Playlist, newName: string): void {
-      playlist.title = newName;
-      this.repository.update(playlist);
+    playlist.title = newName;
+    this.repository.update(playlist);
   }
 }
-
